407f793746a5de17119a067f766b53768b23b856
dklarin <danijel.klarin@gmail.com>
2024-10-23 16:50:41 +0200

    dictionary

---
+++ links.py
@@ -0,0 +1,5 @@
+links = {    
+    '4. MBR, Particije': 'https://carnet-my.sharepoint.com/:b:/g/personal/danijel_klarin_skole_hr/EeJbJkwhqF9EiubGYPLHL-ABYDQh-gIawdFMH59lruZQcQ?e=8CCr7j',
+    '5. Načelo prekida, Dodjeljivač sabirnice, Zahtjev-prihvat, Vektor prekida': 'https://carnet-my.sharepoint.com/:b:/g/personal/danijel_klarin_skole_hr/EeKg_7A-TW1Cp8nw7P8Py2cBxEJ2qi2rDsnnf2Gz3Zj_Fw?e=7Yyiug',
+    '6. Prioritet prekida, Programski prekidi': 'https://carnet-my.sharepoint.com/:b:/g/personal/danijel_klarin_skole_hr/EcnuvNGqQolHuclIkDsD7UwB5ASsPrBx6XXigbQjW7TUag?e=q1fsQU'
+        }
