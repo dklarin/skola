1519aa03f65d9570717c72dd8232f84bb349639c
dklarin <danijel.klarin@gmail.com>
2024-11-14 23:28:16 +0100

    flask-bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from links import *
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 var = 'https://carnet-my.sharepoint.com/:b:/g/personal/danijel_klarin_skole_hr/'
 
@@ -16,6 +18,34 @@ def index():
                            web_dizajn=web_dizajn,
                            flask=flask
                            )
+
+
+
+# flask-bootstrap
+@app.route('/it')
+def informacijske_tehnologije():
+    name = '/it'
+    return render_template('index.html', name=name, links_it=links_it)
+
+# flask-bootstrap
+@app.route('/opsus')
+def operacijski_sustavi():
+    link1 = '/opsus'
+    link2 = '/sjwp'
+    return render_template('index.html', link1=link1, link2=link2, links_it=links_it)
+
+# flask-bootstrap
+@app.route('/sjwp')
+def skriptni_jezici_i_web_programiranje():
+    link1 = '/opsus'
+    link2 = '/sjwp'
+    return render_template('index.html', link1=link1, link2=link2, links_it=links)
+
+# flask-bootstrap
+@app.route('/react-native')
+def react_native():
+    name = 'Župa Svetoga Duha Tisno'
+    return render_template('index.html', name=name, links_it=links)
 
 if __name__ == "__main__":
     app.run(debug=True)
