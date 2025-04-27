58f22c6977fc472e83545bd43e8568f58c1b6f47
dklarin <danijel.klarin@gmail.com>
2025-04-27 16:18:11 +0200

    Sređeno dosta

--- app.py
+++ app.py
@@ -7,38 +7,16 @@ bootstrap = Bootstrap(app)
 
 var = 'https://carnet-my.sharepoint.com/:b:/g/personal/danijel_klarin_skole_hr/'
 
-'''@app.route('/')
-def index():
-    return render_template('cs-for.html', 
-                           var=var,
-                           links_it=links_it,
-                           links=links, 
-                           links_sjwp_u=links_sjwp_u, 
-                           links_sjwp_n=links_sjwp_u,
-                           software=software,
-                           web_dizajn=web_dizajn,
-                           flask=flask
-                           )'''
-
 @app.route('/frame')
 def frame():
     return render_template('frame.html')
 
-# flask-bootstrap
-@app.route('/it')
-def informacijske_tehnologije():
-    title = 'Informacijske tehnologije'
-    return render_template('index.html', var=var, title=title, links_it=links_it)
+link1 = '/sjwp_1'
+link2 = '/pmu_1'
 
-link1 = '/opsus'
-link2 = '/sjwp_1'
-link3 = '/pmu_1'
-link4 = '/wd_1'
-
-subject1 = 'Ne radi još' #'Operacijski sustavi'
-subject2 = 'Skriptni jezici i web programiranje 1'
-subject3 = 'Programiranje mobilnih uređaja'
-subject4 = 'Ne radi još' #'Web dizajn 1'
+info = 'Info stranica'
+subject1 = 'Skriptni jezici i web programiranje 1'
+subject2 = 'Programiranje mobilnih uređaja 1'
 
 # 3. razred
 # flask-bootstrap
@@ -47,93 +25,41 @@ def index():
     title = '9.5.2025. - 2. pisana provjera iz Skriptnih jezika i web programiranja'  
     return render_template('info.html', 
                             var=var,
-                            title=title, 
+                            title=info, 
                             link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
+                            link2=link2,                           
                             subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
+                            subject2=subject2,                           
                             links_it=links_sjwp_1_kodovi,
-                            links_lit=info
-                            )
-
-# 3. razred
-# flask-bootstrap
-@app.route('/opsus')
-def operacijski_sustavi():
-    title = 'Operacijski sustavi'  
-    return render_template('index.html', 
-                            var=var,
-                            title=title, 
-                            link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
-                            subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
-                            links_it=links
+                            links_lit=info_lista
                             )
 
 # flask-bootstrap
 @app.route('/sjwp_1')
-def skriptni_jezici_i_web_programiranje_1():
-    title = 'Skriptni jezici i web programiranje 1'    
+def skriptni_jezici_i_web_programiranje_1():    
     return render_template('index.html', 
                             var=var, 
-                            title=title, 
+                            title=subject1, 
                             link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
+                            link2=link2,                          
                             subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
+                            subject2=subject2,                          
                             links_it=links_sjwp_1_kodovi,
                             links_lit=links_sjwp_1_literatura
                             )
 
-# flask-bootstrap
 @app.route('/pmu_1')
-def react_native():
-    title = 'Programiranje mobilnih uređaja'   
+def programiranje_mobilnih_uredjaja_1():   
     return render_template('index.html', 
                             var=var, 
-                            title=title, 
+                            title=subject2, 
                             link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
+                            link2=link2,                           
                             subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
+                            subject2=subject2,                           
                             links_it=links_sjwp_1_literatura,
                             links_lit=links_pmu_1_literatura
                             )
-
-# flask-bootstrap
-@app.route('/wd_1')
-def web_dizajn_1():
-    title = 'Web dizajn'   
-    return render_template('index.html', 
-                            var=var, 
-                            title=title, 
-                            link1=link1, 
-                            link2=link2, 
-                            link3=link3, 
-                            link4=link4,
-                            subject1=subject1,
-                            subject2=subject2,
-                            subject3=subject3,
-                            subject4=subject4,
-                            links_it=links_web_dizajn_3
-                            )
-
 
 
 if __name__ == "__main__":
--- links.py
+++ links.py
@@ -1,75 +1,30 @@
-links_it = {    
-    '2.1. Prva pomagala za računanje': 'ERrqltsrgQtMqbFEOu4r_gwBeG05XcpWnAOzKPRJUYxUXQ',
-    '2.2. Mehanički strojevi': 'EW6XoAHCgrJJp53EbHc-5-sBENP-lA0B1wwzxaK7uTl6Uw',
-    '2.3. Elektromehanički strojevi': 'EaeZEZvvU9JBsAwisI63UTwBuOdU-BMPv0H-JDLGT-aVzw',
-    '2.4. Elektronička računala': 'EfLHXHT6SL1Mg6vP4w7aXL8BVyg_q_uA_iS2MymJDTI6LQ',
-    '2.5. Osobna računala': 'EfAZqHYJrE9Cjkf9ditARlEBzd7joQCuv2Uqr5-QRq1zFg',
-    '3.1. Računalno sklopovlje': 'EYz6AdfpYlFBjw9zlixE0L0B5ULEP8PuzATahSfSMHT3jA',
-    '3.2. Glavni dijelovi računala': 'Edk705kaRzBBvPnYdZvs1NYByTCQOA7Dij1Ja8y4Jab4Eg',
-    '4.5. Autorska prava i distribucija programa': 'EfC_38Iy4eFLunHQzdeKrsgBL1j3K_E2er55nKsmYO0z0A',
-    '4.6. Zlonamjerni programi': 'EZD56h3fcd9Dvce5tzY2-sgBeG1dUZUJCBFX8VK46K9Xnw',
-    '4.7. Pričuvna pohrana podataka': 'EcIEdV7OifdBtiKsUiVx888BAbz7_dTjQWKUGNl0gCflLA'
-}
-
-software = {
-    'LatencyMon 7.31 (Home Edition)': 'EdYlk4AUohBDvmbQZSBwhDUBolAQTunb4ME31mcWn_BY9g'
-}
-
-
-links = {    
-    '1. Uvod': 'ESjQtcFLbsJOnthinieDf0oBSKcs5uW_vL9TadUCX8avXQ',
-    '2. Pregled operacijskih sustava': 'Ect-CYUsa-9Npuwo0yQuxvMBUs5M6Vdrv2AgvmWAUk3Hmg',
-    '3. Pokretanje': 'EXTKN7RuKKtIlpGus0GUyzsBOCQFOYwl64c6bqbuD-LMiw',
-    '4. MBR, Particije': 'EeJbJkwhqF9EiubGYPLHL-ABYDQh-gIawdFMH59lruZQcQ',
-    '5. Načelo prekida, Dodjeljivač sabirnice, Zahtjev-prihvat, Vektor prekida': 'EeKg_7A-TW1Cp8nw7P8Py2cBxEJ2qi2rDsnnf2Gz3Zj_Fw',
-    '6. Prioritet prekida, Programski prekidi': 'EcnuvNGqQolHuclIkDsD7UwB5ASsPrBx6XXigbQjW7TUag',
-    '7. Program, proces, dretva, raspoređivanje po vremenu i prioritetu ': 'EXHhDnOEyC1GuNbQUEii8H4BM2LF5Smkf4OVMy9HMjIgAw'   
-}
-
-# Prezentacije učenika
-links_sjwp_u = {    
-    '1. Flexbox u CSS-u': 'Eaz2wjZ3oJ1LmyLKWPZXSgQB8cdoSxOpqFJiOb74PScJjw?e=hQhDC8',
-    '2. HTML': 'ESd-HSjMVspDkrZoauuS8BMB2PeBkb5upkj3NlUUc1ooFg?e=MYnOcs',
-    '3. Općenito o CSS-u': 'EX_ZjWSlj2dCm9Y0p396cP4BoNrDer4GpFVVBhry7uPFoA?e=YLUfoM'
-}
-
+# Funkcionalno
 links_sjwp_1_kodovi = { 
-    'Oblikovanje teksta': 'EawhO-ShpfhDkg2aGlLyhm8BijRizwXJbcZxA3pupSuKdQ?e=hrRQHL',
-    'Poveznice': 'EeW38fY2R1FCkIZwK6-pbXIB0zHrH1D-YNgaDc3w_Mx-4A?e=VuiLz7',
-    'Tablice': 'ET3Zx8QirylJkBhmav3PBdkByzXjU-yJ-jP1Uog-tzCCBw?e=wHV5cm',
+    '17. Oblikovanje teksta': 'EawhO-ShpfhDkg2aGlLyhm8BijRizwXJbcZxA3pupSuKdQ?e=hrRQHL',
+    '18. Poveznice': 'EeW38fY2R1FCkIZwK6-pbXIB0zHrH1D-YNgaDc3w_Mx-4A?e=VuiLz7',
+    '19. Tablice': 'ET3Zx8QirylJkBhmav3PBdkByzXjU-yJ-jP1Uog-tzCCBw?e=wHV5cm',
     '20. Slike': 'Ec-AO99Ag11CsVYes47XJzUBua34eCLwl8rwvaXxtSBRlg?e=SXm950',
     '21. Liste': 'ESQ3-rxkN3RHiLZjVu_kdVcBIDeS0MTuTnyvm0yg0s1KYw?e=ZGEhbU',
     '23. Oblikovanje mrežnog sjedišta s pomoću HTML-a': 'ETAlsrndsxdHjnt5qVk4GTABmniGa10kn-WSOFACuuqULA'
 }
 
+# Ovo je funkcionalno
 links_sjwp_1_literatura = {    
-    'Ne radi još': 'EeW38fY2R1FCkIZwK6-pbXIB0zHrH1D-YNgaDc3w_Mx-4A?e=VuiLz7',
-    'Ne radi još': 'ET3Zx8QirylJkBhmav3PBdkByzXjU-yJ-jP1Uog-tzCCBw?e=wHV5cm',
-    'Ne radi još': 'Ec-AO99Ag11CsVYes47XJzUBua34eCLwl8rwvaXxtSBRlg?e=SXm950',
-    '21. Liste': 'ESFgmyg1zExPoEP9weyUcWwBX39WGAR17UntgQWVAyQyQA',
-    '23. Oblikovanje mrežnog sjedišta s pomoću HTML-a': 'EabNYYBkIc5Nhh5L845XWAgBPujKKxz5lRe6YlDAobtReA'
+    '18. Poveznice': 'EavqxejKxPVDm9O8ydXiSG4BgqTe6LvBiR14MN3aA7obMA',
+    '21. Liste': 'EUbaZSksLYhHvC0ozK_ECm4Bkf5c1Ht6kcCcm5YGvBEgGQ',
+    '23. Oblikovanje mrežnog sjedišta s pomoću HTML-a': 'EUPWfoGlWxBGnKR2RIus78ABhYxFQ9ZhRR39JdzlD3H9Mw',
+    '26. Formatiranje multimedijskih sadržaja za primjenu na internetu': 'Ea6nRseex9xIvKXMNtY0aSABCCoDjJJtcBKmgZmbU3Arzg',
+    '30. Vrste CSS-a': 'EZaqHMvJN2RItnGB0oRiNTMBnNE2vC8bQa7iHn17HMZmZw',
+    '31. Sintaksa': 'ERbkEf6uSc1LniyNPj9olygB0UujMZ5UhzuByuPnTXvtmw',
+    '32. Vrste selektora': 'Ec_Ec_-gpN5AqkAQPoJiY4sB0HPioEOylLeaxQXPp8Gvzw',
+    '35. Fontovi': 'EV--C1AF7z1Mv6OKAjHeAaYB67DEKISXqenzL_Sn_VWihQ'
 }
 
+# Ovo je funkcionalno
 links_pmu_1_literatura = {
     '1. Što je React-Native?': 'EbpeuGTrYvBBoJicWDxVCIYB6YZIrdOpdRhAR7es8BMIwg',
     '2. Try-catch': 'ESxrqV5TKoJErMlB1K_piGEBVjm_CxyegVA8GcIF19QQqQ',
     '3. Dozvole': 'EYfXCWegUEdGuUxgWv2jUd4BenqpbCTdMk6yjhcdq__-bQ',
 }
 
-info = [{'predmet': 'Skriptni jezici i web programiranje 1', 'datum': '9.5.2025 (Petak)', 'biljeska': '2. pisana provjera znanja'}]
-
-flask = {
-    '1. Prvi projekt': 'ETehfSJLnjROuFpGUt0ZWcsBGMhwJYFjHkmkmrK4Z_n4vg?e=8pBKbq',
-    '3. Templates 2': 'Edo8EuhqmaZIlbuZ_V1W780Bq8dxj4kVLTWmB_szxdX7OQ'
-}
-
-web_dizajn = {
-    '1. Flask 1: Dinamička ruta, if-else i for': 'Efpp7buw9NhHtZxXixSblXAB7jIzAqIa-dCLDwsGMJ7Ymw?e=fg0KLO'
-}
-
-links_web_dizajn_3 = {
-    '1.1.3. Upoznavanje sa sučeljem': 'EWxGBt47OhpPp1Eazt8aEzABeEtcUacjiaPI3vo0GKo4fw',
-    '1.2. Izrada novog dokumenta': 'EYhn-L3DcjRHktX8kg6YrDcB5eUwSGaEjXIv72RbGUoYDQ'
-}
-
-
+info_lista = [{'predmet': 'Skriptni jezici i web programiranje 1', 'datum': '9.5.2025 (Petak)', 'biljeska': '2. pisana provjera znanja'}]
