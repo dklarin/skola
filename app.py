060d2777a25b2af89d168ed3319e23fd0bbe6753
dklarin <danijel.klarin@gmail.com>
2025-04-27 16:43:40 +0200

    sitnice ispravljene

--- app.py
+++ app.py
@@ -5,63 +5,73 @@ from links import *
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-var = 'https://carnet-my.sharepoint.com/:b:/g/personal/danijel_klarin_skole_hr/'
+var = "https://carnet-my.sharepoint.com/:b:/g/personal/danijel_klarin_skole_hr/"
 
-@app.route('/frame')
+
+@app.route("/frame")
 def frame():
-    return render_template('frame.html')
+    return render_template("frame.html")
 
-link1 = '/sjwp_1'
-link2 = '/pmu_1'
 
-info = 'Info stranica'
-subject1 = 'Skriptni jezici i web programiranje 1'
-subject2 = 'Programiranje mobilnih uređaja 1'
+link1 = "/sjwp_1"
+link2 = "/pmu_1"
+
+info = "Info stranica"
+subject1 = "Skriptni jezici i web programiranje 1"
+subject2 = "Programiranje mobilnih uređaja 1"
+
 
 # 3. razred
 # flask-bootstrap
-@app.route('/')
+@app.route("/")
 def index():
-    title = '9.5.2025. - 2. pisana provjera iz Skriptnih jezika i web programiranja'  
-    return render_template('info.html', 
-                            var=var,
-                            title=info, 
-                            link1=link1, 
-                            link2=link2,                           
-                            subject1=subject1,
-                            subject2=subject2,                           
-                            links_it=links_sjwp_1_kodovi,
-                            links_lit=info_lista
-                            )
+    title = "9.5.2025. - 2. pisana provjera iz Skriptnih jezika i web programiranja"
+    return render_template(
+        "info.html",
+        var=var,
+        title=info,
+        link1=link1,
+        link2=link2,
+        subject1=subject1,
+        subject2=subject2,
+        links_it=links_sjwp_1_kodovi,
+        links_lit=info_lista,
+    )
+
 
 # flask-bootstrap
-@app.route('/sjwp_1')
-def skriptni_jezici_i_web_programiranje_1():    
-    return render_template('index.html', 
-                            var=var, 
-                            title=subject1, 
-                            link1=link1, 
-                            link2=link2,                          
-                            subject1=subject1,
-                            subject2=subject2,                          
-                            links_it=links_sjwp_1_kodovi,
-                            links_lit=links_sjwp_1_literatura
-                            )
+@app.route("/sjwp_1")
+def skriptni_jezici_i_web_programiranje_1():
+    title1 = "1. Kôdovi za 1. i 2. mrežnu stranicu"
+    title2 = "2. Literatura za 2. pisanu provjeru znanja"
+    return render_template(
+        "index.html",
+        var=var,
+        title=subject1,
+        link1=link1,
+        link2=link2,
+        title1=title1,
+        title2=title2,
+        links_it=links_sjwp_1_kodovi,
+        links_lit=links_sjwp_1_literatura,
+    )
 
-@app.route('/pmu_1')
-def programiranje_mobilnih_uredjaja_1():   
-    return render_template('index.html', 
-                            var=var, 
-                            title=subject2, 
-                            link1=link1, 
-                            link2=link2,                           
-                            subject1=subject1,
-                            subject2=subject2,                           
-                            links_it=links_sjwp_1_literatura,
-                            links_lit=links_pmu_1_literatura
-                            )
+
+@app.route("/pmu_1")
+def programiranje_mobilnih_uredjaja_1():
+    return render_template(
+        "index.html",
+        var=var,
+        title=subject2,
+        link1=link1,
+        link2=link2,
+        title1='',
+        title2='',
+        links_it=links_sjwp_1_literatura,
+        links_lit=links_pmu_1_literatura,
+    )
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    #app.run(host='0.0.0.0', port=4000)
+    # app.run(host='0.0.0.0', port=4000)
